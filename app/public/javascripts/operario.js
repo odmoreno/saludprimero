e37e68f8d9b0f0d0467fe8d740ac6d9d3bd5f1f0
CarlosMH95 <cmanosalvas95@gmail.com>
2016-09-05 19:42:07 -0500

    Cambiando el lenguaje de las datatables a español(Operario/muestras)
    Cambiado el lenguaje, aun no se soluciona el boton de ordering

--- app/public/javascripts/operario.js
+++ app/public/javascripts/operario.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
-	$('#tblMuestras').DataTable();
+	$('#tblMuestras').DataTable({
+		language: {
+			processing:     "Procesando...",
+			search:         "Buscar en la Tabla: ",
+			lengthMenu:    "Mostrar _MENU_ Elementos",
+			info:           "Mostrando desde registro  _START_ Hasta registro _END_ De _TOTAL_ ",
+			infoEmpty:      "",
+			infoFiltered:   "(filtrada)",
+			infoPostFix:    "",
+			loadingRecords: "Cargando Registros...",
+			zeroRecords:    "No hay registros",
+			emptyTable:     "No hay registros disponible",
+			paginate: {
+				first:      "	Primero	 ",
+				previous:   "	Anterior	",
+				next:       "	Siguiente	",
+				last:       "	Ultimo	"
+			},
+			aria: {
+				sortAscending:  ": Activar para ordenar de manera ascendente",
+				sortDescending: ": Activar para ordenar de manera descendente"
+			}
+		},
+		"order": [[ 0, "desc" ]]
+
+	} );
 	init();
 	eliminarPaciente();
 	eliminarMuestra();
